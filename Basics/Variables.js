6ee420a570037872e3f12e68fb02bb0fe01410d0
Darshan Parmar <parmardarshanr1@gmail.com>
2025-09-03 11:01:26 +0000

    Variables

---
+++ Basics/Variables.js
@@ -0,0 +1,16 @@
+const account_id = 123456; // integer
+let account_email = "parmardarshanr1@gmail.com"; // string
+var account_pass = "darshan@123"
+account_city = "Ahmedabad"; // string
+let account_state;
+
+
+//count_id = 654321;// not allowed
+//prefer not to use var keyword
+//because it is outdated
+
+account_email = "hc.com"; 
+account_pass = "darshan@1234"
+account_city = "Surat";
+console.log(account_id);
+console.table([account_email, account_pass, account_city, account_state])
