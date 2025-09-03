be803ef15c53ac553856d20ca430ff27a7d142b5
Darshan Parmar <parmardarshanr1@gmail.com>
2025-09-03 11:40:30 +0000

    conversion of datatypes

---
+++ Basics/conversionandoperation.js
@@ -0,0 +1,24 @@
+let score=null;
+
+console.log(typeof score);
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" ==> number 33
+//"33abc" ==> NaN
+//true ==> 1; false ==> 0
+
+let isLoggedIn=0;
+let booleanisloggedin=Boolean(isLoggedIn);
+console.log(booleanisloggedin);
+
+//"" ==> false; "abc"==> true
+//"" ==> false;
+//"darshan"==> true
+
+let someNumber=1234
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof somenumber);
