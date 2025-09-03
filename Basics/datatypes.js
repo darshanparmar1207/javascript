3d564d82fdb8f15f8cc8216270b5257350f11b61
Darshan Parmar <parmardarshanr1@gmail.com>
2025-09-03 11:19:17 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,17 @@
+"use stricts"; //treat all js code as modern js code
+
+let name = "Darshan"; //string
+let age = 24;
+let isApproved = false; //boolean
+
+//number ==> 2 to the power 53
+//string ==> '' or "" 
+//boolean ==> true or false
+//null ==> empty value standlone value
+//undefined ==> value not assigned
+//symbol ==> find unique identifier
+
+//object ==> key value pair
+
+console.log(typeof null)//its contain undifined value
+console.log(typeof undefined)//its objects
